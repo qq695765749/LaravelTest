6b44fe75371c6fdfaf686c7c828f9cd0536ce64e
tom <tom@qq.com>
2018-11-12 11:08:58 +0800

    laravel

---
+++ app/Test1.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Test1 extends Authenticatable
+{
+	protected $table = 'chat';
+	protected $primaryKey = 'id';
+	
+ 	/* public static function test(){
+ 		 return 5566633;
+ 	} */
+}
